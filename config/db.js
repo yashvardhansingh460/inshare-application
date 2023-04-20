379ee2a233e0998d12c773c666e21bdeb70dff63
yashvardhansingh460 <YASHVARDHANSINGH460@GMAIL.COM>
2023-04-21 00:23:18 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+
+function connectDB(){
+    mongoose.connect(process.env.MONGO_CONNECTION_URL);
+    // ,{ useNewUrlParser : true, useCreateIndex:true,useUnifiedTopology:true,
+    //     useFindAndModify: true};
+
+    const connection = mongoose.connection;
+
+    connection.once('open' ,()=>{
+        console.log('Database connected');
+    }).on('error', function(err) {
+    console.log('Connection failed');});
+}
+
+module.exports = connectDB;
+
---
+++ routes/files.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const multer = require('multer');
+const path = require('path');
+const File = require('../models/file');
+const {v4: uuidv4} = require('uuid');
+
+//cb = callback;
+let storage = multer.diskStorage({
+    destination: (req,file,cb) => cb(null,'uploads/'),
+    filename: (req,file,cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
+        cb(null, uniqueName)
+    },
+});
+
+let upload = multer({
+    storage,
+    limits : {fileSize : 1000000*100},
+}).single('myfile');
+
+router.post('/',(req,res)=>{
+    //request is correct or not
+   
+
+   //store a file in upload folder
+  upload(req,res, async (err)=> {
+
+    if(!req.file){
+        return res.json({error : 'there is something error in request..'});
+     }
+
+    if(err) {
+        return res.status(500).send({error : err.message});
+    }
+
+  
+
+
+
+   //store upload folder file into database
+    const file = new File({
+        filename: req.file.filename,
+        uuid : uuidv4(),
+        path : req.file.path,
+        size : req.file.size  
+    });
+const response = await file.save();
+ return res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` });
+});
+
+});
+
+router.post('/send', async (req, res) => {
+    // console.log(req.body);
+    const { uuid, emailTo, emailFrom, expiresIn } = req.body;
+    if (!uuid || !emailTo || !emailFrom) {
+        return res.status(422).send({ error: 'All fields are required except expiry.' });
+    }
+    // Get data from db
+    try {
+        const file = await File.findOne({ uuid: uuid });
+        if (file.sender) {
+            return res.status(422).send({ error: 'Email already sent once.' });
+        }
+        file.sender = emailFrom;
+        file.receiver = emailTo;
+        const response = await file.save();
+        // send mail
+        const sendMail = require('../services/emailService');
+        sendMail({
+            from: emailFrom,
+            to: emailTo,
+            subject: 'inShare file sharing',
+            text: `${emailFrom} shared a file with you.`,
+            html: require('../services/emailTemplate')({
+                emailFrom,
+                downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email`,
+                size: parseInt(file.size / 1000) + ' KB',
+                expires: '24 hours'
+            })
+        }).then(() => {
+            return res.json({ success: true });
+        }).catch(err => {
+            return res.status(500).json({ error: 'Error in email sending.' });
+        });
+    } catch (err) {
+        return res.status(500).send({ error: 'Something went wrong.' });
+    }
+
+
+});
+
+module.exports = router;
