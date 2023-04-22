4e1eea69906f5b14821238977885f77e2d755e0d
yashvardhansingh460 <YASHVARDHANSINGH460@GMAIL.COM>
2023-04-22 10:01:21 +0530

    update cors in server file

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
 //cors
-const corsOption = {
-    origin : process.env.ALLOWED_CLIENTS.split(',')
-}
-app.use(cors(corsOption));
+const corsOptions ={
+    origin:'*',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+app.use(cors(corsOptions));
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine' , 'ejs');
