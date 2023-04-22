5ff280dad5d1d19d23246f30c149e1135ad37013
yashvardhansingh460 <YASHVARDHANSINGH460@GMAIL.COM>
2023-04-22 12:58:31 +0530

    update cors file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const cors = require('cors');
+
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
